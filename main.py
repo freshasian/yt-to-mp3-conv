1be75cdf10ecd944cf7962c8c269f3774233fb4d
Nishan sunar <72113586+freshasian@users.noreply.github.com>
2022-08-22 15:53:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# importing packages
+
+
+# url input from user
+
+
+# extract only audio
+
+
+# check for destination to save file
+
+
+# download the file
+
+
+# save the file
+
+# result of success
